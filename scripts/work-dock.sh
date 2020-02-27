2ae0025f31c265388a2ed7bd6d63d1d2e8dfd623
TheDevTony <tony@thedevtony.com>
2020-02-27 19:10:55 +0000

    Updated config

--- scripts/work-dock.sh
+++ scripts/work-dock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-xrandr --output DP-1-1 --scale 2x2 --right-of eDP-1
-xrandr --output DP-1-3 --scale 1.5x1.5 --left-of eDP-1
+xrandr --output DP1-3 --scale 2x2 --auto --right-of eDP1
+xrandr --output DP1-1 --scale 1.5x1.5 --auto --left-of eDP1
 
 # Seems to only need to be executed once
 # boltctl enroll c2010000-0050-5c0e-03c3-df144603d221
