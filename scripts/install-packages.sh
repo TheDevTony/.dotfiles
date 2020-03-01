fd97a3df36a4478f1fdeaacaa2daf1cf3a30d3e6
TheDevTony <tony@thedevtony.com>
2020-03-01 05:06:18 +0000

    Removed yay install scripts as it's now in pacman

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -4,5 +4,4 @@ sudo pacman -Syu
 sudo pacman -S --needed - < ../packages/packages.txt
 sudo pip install i3ipc
 sh install-python-packages.sh
-sh install-yay.sh
 yay -S --needed - < ../packages/aur-packages.txt
--- scripts/install-yay.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S gcc fakeroot make
-git clone https://aur.archlinux.org/yay.git
-cd yay/
-makepkg -si
-cd ..
-rm -rf yay
